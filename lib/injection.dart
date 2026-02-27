ba7707253dc567a79e455f33eded98d4de2d240e
Satriahrl_ <satriaherlambang22@gmail.com>
2026-02-27 23:02:14 +0700

    feat: mengatur semua dependency jalur koneksi file

--- lib/injection.dart
+++ lib/injection.dart
@@ -3,3 +3,12 @@ import 'package:provider/provider.dart';
 import 'features/cart/data/repositories/cart_repository_impl.dart';
 import 'features/cart/presentation/providers/cart_provider.dart';
 import 'main.dart';
+
+Widget buildApp() {
+  final cartRepository = CartRepositoryImpl();
+
+  return ChangeNotifierProvider(
+    create: (context) => CartProvider(repository: cartRepository),
+    child: const MyApp(),
+  );
+}
