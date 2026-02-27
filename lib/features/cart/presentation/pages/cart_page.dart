73ceada2260b1d899e2c2f567777fcf9d31df370
Satriahrl_ <satriaherlambang22@gmail.com>
2026-02-27 22:37:57 +0700

    class cart page

--- lib/features/cart/presentation/pages/cart_page.dart
+++ lib/features/cart/presentation/pages/cart_page.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart_provider.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var provider = context.watch<CartProvider>();
+
+    return Scaffold(appBar: AppBar(title: const Text('Halaman Keranjang')));
+  }
+}
