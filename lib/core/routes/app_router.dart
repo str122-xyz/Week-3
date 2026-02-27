664433360a0ac9bcfbd35011745facc0405ae334
Satriahrl_ <satriaherlambang22@gmail.com>
2026-02-27 15:09:22 +0700

    feat: setup app router di core layer

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/material.dart';
 import '../../features/cart/presentation/pages/catalog_page.dart';
 import '../../features/cart/presentation/pages/cart_page.dart';
+
+class AppRouter {
+  static const String catalog = '/';
+  static const String cart = '/cart';
+
+  static Map<String, WidgetBuilder> get routes => {
+    catalog: (context) => const CatalogPage(),
+    cart: (context) => const CartPage(),
+  };
+}
