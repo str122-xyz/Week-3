0df4b94ae0a3deec5db4f98161128217e2b657ea
Satriahrl_ <satriaherlambang22@gmail.com>
2026-02-27 15:05:19 +0700

    feat: implement cart repository di data layer

--- lib/features/cart/data/repositories/cart_repository_impl.dart
+++ lib/features/cart/data/repositories/cart_repository_impl.dart
@@ -1,2 +1,18 @@
 import '../../domain/entities/product.dart';
 import '../../domain/repositories/cart_repositories.dart';
+
+class CartRepositoryImpl implements CartRepository {
+  final List<Product> _items = [];
+
+  @override
+  List<Product> getCartItems() => List.unmodifiable(_items);
+
+  @override
+  void addItem(Product product) => _items.add(product);
+
+  @override
+  void removeAllItems() => _items.clear();
+
+  @override
+  bool isItemInCart(String productId) => _items.any((p) => p.id == productId);
+}
