9f3a0650deb4e006fcbc2e2f89631eeb99c70faf
Satriahrl_ <satriaherlambang22@gmail.com>
2026-02-27 22:48:09 +0700

    import an

---
+++ lib/injection.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'features/cart/data/repositories/cart_repository_impl.dart';
+import 'features/cart/presentation/providers/cart_provider.dart';
+import 'main.dart';
