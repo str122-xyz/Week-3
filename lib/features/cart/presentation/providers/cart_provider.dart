b91b1c961ef6f8d0685721babef8191347721357
Satriahrl_ <satriaherlambang22@gmail.com>
2026-02-27 15:14:36 +0700

    class cart provider

--- lib/features/cart/presentation/providers/cart_provider.dart
+++ lib/features/cart/presentation/providers/cart_provider.dart
@@ -1,3 +1,23 @@
 import 'package:flutter/material.dart';
 import '../../domain/entities/product.dart';
 import '../../domain/repositories/cart_repositories.dart';
+
+class CartProvider extends ChangeNotifier {
+  final CartRepository _repository;
+
+  CartProvider({required CartRepository repository}) : _repository = repository;
+
+  List<Product> get items => _repository.getCartItems();
+
+  void addItem(Product product) {
+    _repository.addItem(product);
+    notifyListeners();
+  }
+
+  void removeAll() {
+    _repository.removeAllItems();
+    notifyListeners();
+  }
+
+  bool isInCart(String productId) => _repository.isItemInCart(productId);
+}
