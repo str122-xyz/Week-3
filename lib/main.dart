43ee5a295b301fcc96b20ddf4dc3f2a6a93fa084
Satriahrl_ <satriaherlambang22@gmail.com>
2026-02-27 13:11:16 +0700

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyCatalog(),
+        '/cart': (context) => const MyCart(),
+      },
+    );
   }
 }
