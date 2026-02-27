93b4e1513963c0d9944055371e1376f24d67cbc2
Satriahrl_ <satriaherlambang22@gmail.com>
2026-02-27 15:19:59 +0700

    widget add button 2

--- lib/features/cart/presentation/widgets/add_button_widget.dart
+++ lib/features/cart/presentation/widgets/add_button_widget.dart
@@ -2,3 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../domain/entities/product.dart';
 import '../providers/cart_provider.dart';
+
+class AddButtonWidget extends StatelessWidget {
+  final Product product;
+  const AddButtonWidget({required this.product, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isInCart = context.select<CartProvider, bool>(
+      (provider) => provider.isInCart(product.id),
+    );
+
+    return TextButton(
+      onPressed: isInCart
+          ? null
+          : () => context.read<CartProvider>().addItem(product),
+      child: isInCart
+          ? const Icon(Icons.check, color: Colors.green)
+          : const Text('Tambah'),
+    );
+  }
+}
